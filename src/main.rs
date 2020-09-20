ea48f827feb905608dddc44a77ad7c4ad5647c1d
Sir Matrix <killer20816@gmail.com>
2020-09-20 10:40:41 +0000

    Added directroy searching as well as added curl to our cargo file for later usage.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+
+use std::fs;
+use std::path::Path;
+
+
+pub fn check_existance(){
+    let remotep = "ofslinux-server-launcher/remote";
+    let localp =  "ofslinux-server-launcher/local";
+    let remote = Path::new(&remotep).exists();
+    let local = Path::new(&localp).exists();
+
+    if remote != true{
+        fs::create_dir_all(remotep).unwrap();
+    }
+    if local != true{
+        fs::create_dir_all(localp).unwrap();
+    }
+
+}
+
+
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("Test");
+    check_existance();
+
 }
